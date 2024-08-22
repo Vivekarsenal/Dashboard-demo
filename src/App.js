1b072b3882643e0e53a611f85ea5afcde01baee4
Vivekarsenal <IT20063@glbitm.ac.in>
2024-08-22 22:20:15 +0530

    adding dashboard component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 // src/App.js
 import React, { useState } from 'react';
-// import Dashboard from './Dashboard.js';
-import Navbar from './Navbar.jsx';
-
+import Dashboard from './Dashboard.js';
 
 
 
 const App = () => {
   // Initial categories and widgets data
-  // /const [categories, setCategories] = useState([
-  //   {
-  //     name: "CSPM Executive Dashboard",
-  //     widgets: [
-  //       { id: 1, name: "Widget 1", text: "This is widget 1 content." },
-  //       { id: 2, name: "Widget 2", text: "This is widget 2 content." }
-  //     ]
-  //   },
-  //   {
-  //     name: "Security Dashboard",
-  //     widgets: [
-  //       { id: 3, name: "Widget 3", text: "This is widget 3 content." }
-  //     ]
-  //   }
-  // ]); 
+  const [categories, setCategories] = useState([
+    {
+      name: "CSPM Executive Dashboard",
+      widgets: [
+        { id: 1, name: "Widget 1", text: "This is widget 1 content." },
+        { id: 2, name: "Widget 2", text: "This is widget 2 content." }
+      ]
+    },
+    {
+      name: "Security Dashboard",
+      widgets: [
+        { id: 3, name: "Widget 3", text: "This is widget 3 content." }
+      ]
+    }
+  ]);
 
   return (
     <div className="App">
-     This is a Dashboard sample
-     
-       {/* <Navbar />  */}
-      
+      <Dashboard categories={categories} />
     </div>
   );
 };
